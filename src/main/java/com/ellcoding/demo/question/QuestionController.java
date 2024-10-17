b0e3d00329e521dbbc68c84fca66a0a2fdbd88bf
Koryjlee <koryjlee@gmail.com>
2024-10-17 19:34:42 +0900

    question url 수정

--- src/main/java/com/ellcoding/demo/question/QuestionController.java
+++ src/main/java/com/ellcoding/demo/question/QuestionController.java
@@ -1,14 +1,13 @@
 package com.ellcoding.demo.question;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class QuestionController {
 
-    @GetMapping("/questoin/list")
+    @GetMapping("/question/list")
     @ResponseBody
     public String list() {
         return "question list";
